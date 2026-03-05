89396f4ee8e69bf1d0f155035de9f816fe78f305
Anush Manuja <anushmanuja@gmail.com>
2026-03-05 12:02:09 +0530

    Add Public Exercise Access microservice

---
+++ public-exercise-service/public-exercise-service/src/main/java/com/physio/publicexercise/PublicExerciseServiceApplication.java
@@ -0,0 +1,13 @@
+package com.physio.publicexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicExerciseServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublicExerciseServiceApplication.class, args);
+	}
+
+}
